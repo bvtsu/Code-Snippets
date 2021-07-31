2c55978c1e46368bf92539de406217af79224b58
Brian Tsu <tsu.brian.v@gmail.com>
2021-07-31 16:10:45 -0700

    data by GOterm to violin plot

---
+++ Python/Violin-Plot/GO-violin-plot.py
@@ -0,0 +1,16 @@
+# library & dataset
+import seaborn as sns
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(sys.argv[1])
+GOterm=str(sys.argv[3])
+
+df_subset = df[df["GOBiologicalProcess"].str.contains(GOterm)]
+df_subset['Label']=GOterm
+
+# plot
+sns_plot=sns.violinplot(x=df_subset[sys.argv[2]], y=df_subset['Label'])
+plt.title('{} {}'.format(GOterm,sys.argv[2]))
+sns_plot.figure.savefig('{}_{}.png'.format(GOterm,sys.argv[2]))
---
+++ Python/Violin-Plot/README.md
@@ -0,0 +1,4 @@
+Takes CSV file containing gene info per row. 
+
+1) One column must contain GO terms associated with the gene.
+2) Other columns must contain int or floats.
